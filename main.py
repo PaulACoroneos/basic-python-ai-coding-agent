de9e5b204f64ad146102836238d1986177071954
PaulACoroneos <paul.coroneos@icloud.com>
2026-04-16 22:03:05 -0500

    format

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     if not api_key:
-        raise RuntimeError('Could not load gemini api key')
-    
+        raise RuntimeError("Could not load gemini api key")
+
     client = genai.Client(api_key=api_key)
 
-    content = client.models.generate_content(model='gemini-2.5-flash',contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    
+    content = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
     print(content.text)
+
 
 if __name__ == "__main__":
     main()
